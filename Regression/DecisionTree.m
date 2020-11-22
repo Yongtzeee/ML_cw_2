74f175bf88d91e6c1984838d42a12b0891dc487c
Aimi27 <55775491+Aimi27@users.noreply.github.com>
2020-11-22 07:30:49 +0000

    Add DecisionTree.m

--- Regression/DecisionTree.m
+++ Regression/DecisionTree.m
@@ -1,1 +1,11 @@
-test
+function DecisionTree(taskType)
+
+% load the datasets
+data_redwine = readtable('winequality-red.csv');
+data_whitewine = readtable('winequality-white.csv');
+
+% split dataset into features and labels
+features_red = data_redwine(:, 1:size(data_redwine, 2)-1);
+features_white = data_whitewine(:, 1:size(data_whitewine, 2)-1);
+labels_red = data_redwine(:, size(data_redwine, 2));
+labels_white = data_whitewine(:, size(data_whitewine, 2));
