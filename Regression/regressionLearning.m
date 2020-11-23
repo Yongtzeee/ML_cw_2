6ac513a1148da9fa52f1a874354814976fbe81d0
JiachennCJC <jiachenn99@gmail.com>
2020-11-23 11:27:57 +0000

    Modified file loading, WIP learning algo;

---
+++ Regression/regressionLearning.m
@@ -0,0 +1,12 @@
+function regressionTree = regressionLearning(features, labels)
+    
+% Inputs
+%   features -> N x d training examples
+%   labels -> N x 1 target labels for training examples
+
+% initialize tree structure
+tree.best_attribute = 0;
+tree.threshold = 0;
+
+
+end
