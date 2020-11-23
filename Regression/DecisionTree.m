2de1ed68031b742ba371284389446510da1a72f8
Aimi27 <55775491+Aimi27@users.noreply.github.com>
2020-11-23 06:39:54 +0000

    Add files via upload

--- Regression/DecisionTree.m
+++ Regression/DecisionTree.m
@@ -6,3 +6,9 @@ data = readtable('winequality-red_white.csv');
 % split dataset into features and labels
 features = data(:, 1:size(data, 2)-1);
 labels = data(:, size(data, 2));
+
+% split dataset into train and test datasets
+featuresTrain = features(1:3250, :);
+featuresTest = features(3251:size(features, 1), :);
+labelsTrain = labels(1:3250, :);
+labelsTest = labels(3251:size(labels, 1), :);
