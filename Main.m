08c35e65267bf8a64dfbf1dd2dac65061614e81d
JiachennCJC <jiachenn99@gmail.com>
2020-12-02 18:20:00 +0000

    Update Main.m

--- Main.m
+++ Main.m
@@ -1,11 +1,13 @@
 function Main(taskType)
 
 if taskType == 0
+    addpath(genpath('Classification/'))
     data = readtable('online_shoppers_intention.csv');
     classificationTree(data)
     
     
 elseif taskType == 1
+    addpath(genpath('Regression/'))
     red_data = readtable('winequality-red.csv', 'PreserveVariableNames', 1);
     white_data = readtable('winequality-white.csv', 'PreserveVariableNames', 1);
     combined_data = [red_data;white_data];
