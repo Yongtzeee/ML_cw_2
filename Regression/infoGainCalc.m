c17e7fa96bfa83b4a4fe1116c6b6775760708232
JiachennCJC <jiachenn99@gmail.com>
2020-12-02 11:39:03 +0000

    Fixed, should return proper;y

--- Regression/infoGainCalc.m
+++ Regression/infoGainCalc.m
@@ -1,67 +1,82 @@
-function [bestThreshold, bestIndex] = infoGainCalc(data)
+function [bestThresholdsList, freqDict, labelStore] = infoGainCalc(training)
+    % Returns an array of bestThresholds of all features, 
+    % shape -> [1x11]
 
     % Using a method like a dictionary in python to get counts for all
     % unique values, then calcualte their stddev
-
-    columns = data.Properties.VariableNames();
+    columns = training.Properties.VariableNames();
     
     % Shape of our data, rows x columns
-    dataShape = size(data);
+    [m, n]= size(training);
     
-    % freqDict will look like {uniquekey:count} pairing after
-    % assignment
+    % freqDict will look like {key:count} 
     sdrList = [];
     thresholdList = [];
-    datasetStdev = std(data{:, dataShape(2)});
-
-    for feature = 1:length(columns) %Loop through all our features first   
+    bestThresholdsList = [];
+    labelsStd = std(training{:, n}); %stdev of the labels
+   
+    bestSDR = 0;
+    bestThreshold = 0;
+    bestIndex = 0;
+    bestFeature = 0;
+    
+    for feature = 1:length(columns)-1 %Loop through all our features first except the label in last col 
+        disp("NOW ON FEATURE "+feature)
         sdrFinal = 0;
-        disp("Currently on feature: "+feature)
-        featureCol = table2array(data(:, columns(feature)));
-        uniqueValues = unique(featureCol); %unique values of the particular feature 
-        uniqueValuesSize = size(uniqueValues);
-        freqDict = zeros(uniqueValuesSize(1),2);
-        freqDict(:,1) = uniqueValues; % Assign keys to freqDict
+        sdaResult = 0;
+        stdev = [];
         
         labelStore = []; 
+        freqDict = [];
+%         featureCol = table2array(data(:, columns(feature))); %
         
-        for row = 1:dataShape(1) %Loop through rows of table data to do counting
-            disp("Row: "+row)
-            for uniqueVal = 1:length(uniqueValues)
-               if data{row,feature} == uniqueValues(uniqueVal,1)
-                   freqDict(uniqueVal, 2) = freqDict(uniqueVal,2) + 1; % Increment count if found same one
-                   labelStore(freqDict(uniqueVal, 2),uniqueVal) = data{row, dataShape(2)}; 
+%         uniqueValues = unique(featureCol); %unique values of the particular feature 
+        uniqueValues = unique(training{:,feature});
+        [uniqueValuesRow, uniqueValuesCol] = size(uniqueValues);
+
+        freqDict = zeros(uniqueValuesRow,2);
+        freqDict(:,1) = uniqueValues; 
+        
+        for i = 1:m %Loop through rows of table data to do counting
+            for j = 1:uniqueValuesRow
+               if training{i,feature} == freqDict(j,1)
+                   freqDict(j, 2) = freqDict(j,2) + 1; % Increment count encountering the same key
+                   labelStore(freqDict(j, 2),j) = training{i, n}; 
                    break   	         
                end
            end
         end
-        
-        stdev = std(labelStore, "omitNan");
+
+        % Calculate stdev of labels pertaining to this feature
+        labelStore(labelStore==0)=NaN;
+        stdev = std(labelStore,'omitnan');
         [min_stdev, index] = min(stdev);
-        threshold = freqDict(index,1);
-        thresholdList(feature) = threshold;
+        threshold = freqDict(index,1); % get the best threshold value
         
         % Calculate SDA and SDR
-        sdrFinal = datasetStdev - sda(freqDict, stdev, data);
-        sdrList(feature) = sdrFinal;   
+        sdaResult = sda(freqDict, stdev, training);
+        sdrFinal = labelsStd - sdaResult;
+        
+        if sdrFinal > bestSDR
+            bestSDR = sdrFinal;
+            bestThreshold = threshold;
+            bestFeature = feature;
+            bestIndex = feature;
+        end
+        bestThresholdsList(feature) = bestThreshold;
+        
+        disp("Current best SDR: "+bestSDR)
+        disp("Current best threshold: "+bestThreshold)
+        disp("Current best feature: "+bestFeature)
+        disp("Current best index: "+bestIndex)
     end
-
-    % Selecting the optimal threshold and sdr (max)
-    [bestSDR, bestIndex] = max(sdrList);
-    bestThreshold = thresholdList(bestIndex);
-    
-    if bestSDR < 0
-        bestThreshold = -Inf;
-        bestIndex = -1;
-    end
-    
 end
+
 
 function sdaResult = sda(freqDict,datasetSD,data)
     sdaResult = 0;
     [m,n] = size(data);
     [a,b] = size(freqDict);
-    
     for i = 1:a
         sdaResult = sdaResult + freqDict(i,2)/m * datasetSD(i);
     end
