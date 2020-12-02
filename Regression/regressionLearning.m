569550ce70b4aa43b04708a3bd2006e17c3b675d
Aimi27 <55775491+Aimi27@users.noreply.github.com>
2020-12-02 13:07:36 +0000

    Add files via upload

--- Regression/regressionLearning.m
+++ Regression/regressionLearning.m
@@ -13,57 +13,36 @@ tree.prediction = -1;
 tree.attribute = -1;
 tree.threshold = 0;
 
-% if all target labels are the same, then return the subtree as leaf node
-% labelMat = table2array(labels)';
-% if all(labelMat == labelMat(1))
-%     tree.prediction = labelMat(1);
-%     regressionTree = tree;
-%     
-labelMat = table2array(data(:,size(data, 2)));
+labelMat = table2array(data(:,size(data, 2))); % extract labels
 disp("Label mat size: "+size(labelMat))
-if all(labelMat == labelMat(1)) 
+
+if all(labelMat == labelMat(1)) % if all labels are the same, return as leaf
     tree.prediction = labelMat(1);
     regressionTree = tree;
     
-else
+else % different labels
     [bestThresholdsList, bestIndex] = infoGainCalc(data);
-%     for i = i:length(features)
-%         [bestThreshold, bestAttribute] = infoGainCalc();
-
     disp("Best attribute is attribute: " + bestIndex)
 
-    % if max depth is exceeded, choose labels as leaf node
-    % else do below
-    if depth == 6
+    if depth > maxDepth
+        % if leaf node
         tree.prediction = labelMat(:, size(data,2));
         regressionTree = tree;
-%     elseif depth > maxDepth
-%         tree.prediction = majorityValue(labels);
-%         regressionTree = tree;
     else
         depth = depth + 1;
         disp("Current depth: " + depth)
         % is not leaf node
         tree.op = data.Properties.VariableNames(bestIndex);
         tree.attribute = bestIndex;
-        tree.threshold = bestThreshold;
+        tree.threshold = bestThresholdsList;
 
         % retrieve datapoints that have bestAttribute < bestThreshold     
-        
-        dataRows = table2array(data(:, bestIndex)) < bestThreshold;
+        dataRows = table2array(data(:, bestIndex)) < bestThresholdsList;
         dataLeft = data(dataRows, :);
-        
-%         featureRows = table2array(data(:, bestAttribute)) < bestThreshold;
-%         featuresLeft = data(featureRows, :);
-%         labelsLeft = labels(featureRows, :);
 
         % retrieve datapoints that have bestAttribute >= bestThreshold
-        dataRows = ~dataRows;
+        dataRows = table2array(data(:, bestIndex)) >= bestThresholdsList;
         dataRight = data(dataRows, :);
-        
-%         featureRows = ~featureRows; % bestAttribute >= bestThreshold is exact opposite of bestAttribute < bestThreshold
-%         featuresRight = data(featureRows, :);
-%         labelsRight = labels(featureRows, :);
 
         % create children
         leftChild = regressionLearning(dataLeft, depth);
